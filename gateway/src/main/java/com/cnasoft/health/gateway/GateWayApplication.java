aceeeab9254c899c91258dd7ee98f1613512d336
zjh <2874204690@qq.com>
2022-10-17 20:07:33 +0800

    init

---
+++ gateway/src/main/java/com/cnasoft/health/gateway/GateWayApplication.java
@@ -0,0 +1,17 @@
+package com.cnasoft.health.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableZuulProxy
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
