309658b5c7b7a50126acc2e1ce2e46390eacd879
zjh <2874204690@qq.com>
2022-10-17 20:58:51 +0800

    init

---
+++ gateway/src/main/java/com/cnasoft/health/gateway/route/constant/Constant.java
@@ -0,0 +1,21 @@
+package com.cnasoft.health.gateway.route.constant;
+
+/**
+ * @author cnasoft
+ * @date 2020/9/22 19:40
+ */
+public interface Constant {
+    /**
+     * 配置中心标识
+     * 动态路由配置格式：
+     * [
+     *     {
+     *         "id":"api-user",
+     *         "path":"/api-user/**",
+     *         "serviceId":"user-service"
+     *     }
+     * ]
+     */
+    String ZUUL_DATA_ID = "zuul-routes-toc";
+    String ZUUL_GROUP_ID = "ZUUL_GATEWAY_TOC";
+}
